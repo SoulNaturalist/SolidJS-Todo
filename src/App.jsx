994f18c2163f4cd8e6081cabfd4faf4bde8c1537
SoulNaturalist <omg12121@mail.ru>
2024-09-04 09:22:22 +0300

    added beautiful text-shadow??

--- src/App.jsx
+++ src/App.jsx
@@ -80,10 +80,10 @@ function App() {
                   className={`task-${task[0]}`} 
                   onClick={(e) => swapStatus(e.currentTarget.className.replace("task-", ""))}
                 >
-                  <div style={{textAlign:"center", display:"block", margin:"auto", background:task[1] ? "green":"red", color:"white", "border-radius":"10px", padding:"15px"}}>
+                  <div style={{textAlign:"center", display:"block", margin:"auto", background:task[1] ? " #1a1a1a":"#fff", color:task[1] ? "#f0f0f0":"#000", "border-radius":"10px", padding:"15px", "text-shadow":"0 0 15px rgba(255,255,255,.5), 0 0 10px rgba(255,255,255,.5)", display:"inline-block"}}>
                     Задание: {task[0]}
                     <br/>
-                    статус {task[1] ? "Завершено": "В процессе"}
+                    Статус <p style={{color:task[1] ? "green":"#000", display:"inline-block", "text-shadow": !task[1] ? "3px 5px 2px #474747":""}}>{task[1] ? "Завершено": "В процессе"}</p>
                   </div>
                 </div>
                 <div style={{textAlign:"center", display:"block", margin:"auto", background:"grey", color:"white", cursor:"pointer","border-radius":"5px", padding:"5px"}} onClick={() => setId(index)}>Изменить</div>
